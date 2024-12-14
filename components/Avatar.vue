1ed3c11545e35e49661bbd43b181a617d11295df
Jalil Abdullayev <abdullacelil00@gmail.com>
2024-12-14 14:13:37 +0400

    Create app design
    - create Avatar component for logo or profile picture

---
+++ components/Avatar.vue
@@ -0,0 +1,3 @@
+<template>
+  <img src="~/assets/images/logo.svg" alt="logo coding space" class="size-24 rounded-full"/>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-gradient-to-br from-red-400 to-sky-200 w-screen h-screen">
+    <div class="flex justify-center items-center pt-4">
+      <Avatar/>
+    </div>
+    <div class="text-center text-lg font-medium mt-2 pb-4 text-white">
+      <h1>
+        Full Stack Web Developer
+      </h1>
+      <h2>
+        Jalil Abdullayev
+      </h2>
+    </div>
+  </div>
+</template>
