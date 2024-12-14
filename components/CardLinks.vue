fddea3c304b8f559c70507315662e4eced49842f
Jalil Abdullayev <abdullacelil00@gmail.com>
2024-12-14 14:53:51 +0400

    Create Card Links component

---
+++ components/CardLinks.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps({
+  link: {
+    type: Object
+  }
+})
+</script>
+
+<template>
+  <a :href="link.link" target="_blank"
+     class="bg-white flex items-center rounded-full p-4 w-full mx-4 max-w-md shadow-lg shadow-red-500">
+    <Icon :name="`mdi:${link.icon}`" size="32" class="text-red-600 -mr-8"/>
+    <p class="font-bold px-2 m-auto">
+      {{ link.text }}
+    </p>
+  </a>
+</template>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,29 @@
+<script setup lang="ts">
+interface Link {
+  link: string,
+  text: string,
+  icon: string
+}
+
+const links: Link[] = [
+  {
+    link: 'https://youtube.com/@Jalil_Abdullayev',
+    text: 'YouTube',
+    icon: 'youtube'
+  },
+  {
+    link: 'https://instagram.com/jalil_abdullayev_',
+    text: 'Instagram',
+    icon: 'instagram'
+  },
+  {
+    link: 'https://linkedin.com/in/jalil-abdullayev/',
+    text: 'LinkedIn',
+    icon: 'linkedin'
+  }
+];
+</script>
+
 <template>
   <div class="bg-gradient-to-br from-red-400 to-sky-200 w-screen h-screen">
     <div class="flex justify-center items-center pt-4">
@@ -10,6 +36,9 @@
       <h2>
         Jalil Abdullayev
       </h2>
+    </div>
+    <div class="flex flex-col items-center px-4">
+      <CardLinks v-for="(link, index) in links" :key="index" :link="link" class="mt-6"/>
     </div>
   </div>
 </template>
